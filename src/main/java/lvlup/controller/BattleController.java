d65b3a94ca56884ce058c95fe36038296c96a8db
Meike <meike_hankewicz@yahoo.de>
2018-11-06 12:11:28 +0100

    explicit declaration of json responses for swagger

--- src/main/java/lvlup/controller/BattleController.java
+++ src/main/java/lvlup/controller/BattleController.java
@@ -14,22 +14,22 @@ public class BattleController {
     @Autowired
     private BattleService battleService;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public List<Battle> getBattles(@PathVariable String name){
         return battleService.listAll(name);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
     public Battle getBattle(@PathVariable String name, @PathVariable Long id){
         return battleService.getBattle(name, id);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
     public Battle createBattle(@PathVariable String name, @RequestBody Battle battle){
         return battleService.create(name, battle);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "application/json")
     public void deleteBattle(@PathVariable String name, @PathVariable Long id){
         battleService.delete(name, id);
     }
--- src/main/java/lvlup/controller/SkillController.java
+++ src/main/java/lvlup/controller/SkillController.java
@@ -8,34 +8,34 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("skills")
+@RequestMapping("/skills")
 public class SkillController {
 
     @Autowired
     private SkillService skillService;
 
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     public List<Skill> getSkills(){
         return skillService.listAll();
     }
 
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET, produces = "application/json")
     public Skill getSkill(@PathVariable String name){
         return skillService.getSkill(name);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
     public Skill createSkill(@RequestBody Skill skill){
         return skillService.create(skill);
     }
 
-    @RequestMapping(value = "/{name}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/{name}", method = RequestMethod.PUT, produces = "application/json")
     public Skill updateSkill(@PathVariable String name, @RequestParam int xp){
         return skillService.updateXp(name, xp);
     }
 
-    @RequestMapping(value = "/{name}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/{name}", method = RequestMethod.DELETE, produces = "application/json")
     public void deleteSkill(@PathVariable String name){
         skillService.delete(name);
     }
