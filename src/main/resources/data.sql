501be7043b61cefc25656c3bda1f57534b7ccc21
Meike <meike_hankewicz@yahoo.de>
2019-03-22 10:46:16 +0100

    added initial data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO skills(ID, name, curr_lvl, max_lvl, curr_xp) VALUES (1, 'Programming', 5, 20, 30);
+INSERT INTO battles(ID, description, xp, fk_skill) VALUES (1, '30 minutes freeCodeCamp', 20, 1);
+INSERT INTO battles(ID, description, xp, fk_skill) VALUES (2, 'Java Tutorial', 50, 1);
